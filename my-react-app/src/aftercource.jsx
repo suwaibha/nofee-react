29fb2634cd167238cd79e71575bd0f1e82442e6e
suwaibha <suwaibharauf08@gmail.com>
2025-04-25 02:53:20 +0530

    Update aftercource.jsx

--- my-react-app/src/aftercource.jsx
+++ my-react-app/src/aftercource.jsx
@@ -1,7 +1,7 @@
 const Aftercource = () => {
     return(
         <div className="min-h-screen flex items-center justify-center bg-white">
-            <div className="bg-white p-8 rounded-xl shadow-md w-full max-w-150 border-1 h-90">
+            <div className="bg-white p-8 rounded-xl shadow-md w-full max-w-150 border-1 h-110">
                 <p className="text-xl font-bold text-center mb-6">
                  <h1>Congratulations! 🎉 </h1>
                 </p>
@@ -15,7 +15,7 @@ const Aftercource = () => {
                 <div className="items-center text-center"> 
                 We also value continuous growth! Provide your feedback to help us improve and enhance the experience for future candidates. 😊
                 </div>
-                <div className="items-center text-center space-x-2.5">
+                <div className="items-center text-center space-x-2.5 p-2.5">
 
                 <button onClick={()=>alert("Your Feedback Has Been Submitted Successfully") }
                 className="font-light text-white bg-blue-500 hover:bg-blue-900 border-1 rounded-2xl p-2 border-black py-1 mt-4">
